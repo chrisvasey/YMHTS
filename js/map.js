6ecef04c69eab6516584b2c35c9ae28b2c5b2eb6
chrisvasey <chrismvasey@gmail.com>
2015-01-25 23:57:50 +0000

    Added buttons and started adding the click functions

--- js/map.js
+++ js/map.js
@@ -1,3 +1,9 @@
 L.mapbox.accessToken = 'pk.eyJ1IjoiY2hyaXNpdGFsaWsiLCJhIjoiQm1TaFdMZyJ9.rApsbB6i-PoSWKnBfeaNsg';
 var map = L.mapbox.map('map', 'chrisitalik.l1nnb1l2')
     .setView([53.953, -1.082], 13);
+
+$(document).ready(function(){
+    $("#back").click(function(){
+        alert("this click worked");
+    });
+});
