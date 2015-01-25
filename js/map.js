d2971ce50779230febe6088f5cd97471cb19f13b
chrisvasey <chrismvasey@gmail.com>
2015-01-25 23:37:00 +0000

    added map

---
+++ js/map.js
@@ -0,0 +1,3 @@
+L.mapbox.accessToken = 'pk.eyJ1IjoiY2hyaXNpdGFsaWsiLCJhIjoiQm1TaFdMZyJ9.rApsbB6i-PoSWKnBfeaNsg';
+var map = L.mapbox.map('map', 'chrisitalik.l1nnb1l2')
+    .setView([53.953, -1.082], 13);
